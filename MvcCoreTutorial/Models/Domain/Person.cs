ef7566bee87d867afd2490f9147ea9a006417651
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-07-02 16:47:49 +0800

    Adding boostrap to the layout and navbar

---
+++ MvcCoreTutorial/Models/Domain/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcCoreTutorial.Models.Domain
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
