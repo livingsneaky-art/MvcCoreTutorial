35bba65ab32ade2265614a06ddbb4fa33c568e1a
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-07-02 20:01:20 +0800

    Fix/established sql connection string, and adding a builder for db using the entity framework

--- MvcCoreTutorial/Program.cs
+++ MvcCoreTutorial/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MvcCoreTutorial.Models.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DatabaseContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
+//whatever services are needed in application will be declared here
 
+//...................................
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
