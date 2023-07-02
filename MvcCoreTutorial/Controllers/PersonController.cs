f27a6ed8bafcdc2fa6298a0194d651d2934c0979
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-07-02 15:49:45 +0800

    Add project files.

---
+++ MvcCoreTutorial/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreTutorial.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ViewBag and View Data can send data only from ControllerToView
+            ViewBag.greeting = "Hello World!";
+            ViewData["greeting2"] = "I am using ViewData";
+            
+
+            //Tempdata can send data from one controller method to another controller method
+            TempData["greeting3"] = "It's TempData msg";
+            return View();
+        }
+    }
+}
